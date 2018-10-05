6393642dc21816ffd2aedc4ba1f608635669af2a
stabbysaurus <vicious.narcoleptic@gmail.com>
2018-10-04 19:06:30 -0500

    Finished the tabular Q example!

--- tabular_q.py
+++ tabular_q.py
@@ -31,4 +31,43 @@ There are 16 possible states (4x4 grid of blocks) and 4 possible actions (direct
 In total, we need a 16x4 table of Q-values (state, action pairs).
 """
 
-Q = np.zeros([env.observation_space.n, env.action_space.n])
+def tabular_Q():
+
+    """this function learns the values of each state/action by manually setting up
+    a table of all (state, action) pairs and updating the rewards received."""
+
+    Q = np.zeros([env.observation_space.n, env.action_space.n])
+
+    """
+    Training loop!
+    """
+    lr = 0.8
+    gamma = 0.95
+    n_steps = 2000
+    sub_steps = 100
+
+    rewards = []
+    for i in range(n_steps):
+        cur_state = env.reset()  # resets and returns first observation
+        cur_reward = 0.  # episode reward
+
+        for j in range(sub_steps):
+
+            """greedily pick the highest-value action with random normal noise"""
+            cur_action = np.argmax(Q[cur_state, :] + np.random.randn(1, env.action_space.n) * (1./ (i + 1)))
+            next_state, reward, doom, _ = env.step(cur_action)
+
+            """update the Q-table with the reward received.
+            note that this is structured as the surprise: 
+            the old Q-value is shifted by the learning rate * difference in reward received and reward expected."""
+            Q[cur_state, cur_action] += lr * (reward + gamma * np.max(Q[next_state, :]) - Q[cur_state, cur_action])
+
+            cur_state = next_state
+            cur_reward += reward
+
+            """if doom (agent fell into a hole), the episode ends"""
+            if doom:
+                break
+
+        rewards.append(cur_reward)
+        print(sum(rewards) / (i + 1.))  # track average reward per episode
